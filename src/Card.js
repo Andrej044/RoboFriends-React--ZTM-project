8a473b498548c12bac47114a29d9551c31375e1a
Andrii Lishchyna <andrej044@i.ua>
2023-06-09 19:48:08 -0400

    add alt atribute to img

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
  const Card = () => {
   return(
     <div>
-      <img src="" />
+      <img src="" alt ="robot"/>
       <h2>John Doe</h2>
       <p>johndoe@gmail.com</p>
     </div>
