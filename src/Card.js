5b5a387de673702a43fcd1280b99f1710af22b73
Andrii Lishchyna <andrej044@i.ua>
2023-06-09 19:55:35 -0400

    add value to src attribute of img's tag

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
  const Card = () => {
   return(
     <div>
-      <img src="" alt ="robot"/>
+      <img src="https://robohash.org/test?200x200" alt ="robot"/>
       <h2>John Doe</h2>
       <p>johndoe@gmail.com</p>
     </div>
