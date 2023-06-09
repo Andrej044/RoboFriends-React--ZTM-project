f1627943d20f21349f11de223d300433ec6b2b0e
Andrii Lishchyna <andrej044@i.ua>
2023-06-09 19:44:37 -0400

    create Card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+ const Card = () => {
+  return(
+    <div>
+      <img src="" />
+      <h2>John Doe</h2>
+      <p>johndoe@gmail.com</p>
+    </div>
+  )
+}
+
+
+export default Card;
