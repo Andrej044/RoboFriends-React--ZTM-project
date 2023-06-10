4b00b0e288111fe5ee059ce5ae87be7ef11dde60
Andrii Lishchyna <andrej044@i.ua>
2023-06-09 20:30:50 -0400

    map robots list and render to page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import {robots} from './robots'
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const robotsList = robots.map(robot => {
+  return(
+    <Card id = {robot.id} 
+          name = {robot.name}
+          username = {robot.username}
+          email = {robot.email}
+    />  
+  )
+})
+
 root.render(
   <React.StrictMode>
-    <Card />
+    {robotsList}
   </React.StrictMode>
 );
 
