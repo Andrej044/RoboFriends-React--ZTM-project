1c1ab1456e4580316e0b4effa715295a568fc050
Andrii Lishchyna <andrej044@i.ua>
2023-06-10 13:42:18 -0400

    add state, transformate function component to class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import RobotsList from "./RobotsListComponent";
 import SearchBox from './SearchBox'
+import {robots} from './robots'
 
-const App = ({robots})=> {
-  return(
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <RobotsList  robots = {robots} />
-    </div>
-  )
+const state = {
+  robots: robots,
+  searchfield: '',
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return(
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <RobotsList  robots = {robots} />
+      </div>
+    )
+  }
+}
