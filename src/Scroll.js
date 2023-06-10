85cc8b8da4159380d6ed0d4f7a8f25dbd3a33452
Andrii Lishchyna <andrej044@i.ua>
2023-06-10 15:18:07 -0400

    make  a component scrollable

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 
-const Scroll = () =>{
+const Scroll = (props) =>{
   return(
-    <h1>Hi</h1>
+    <div className="overflowComponent">
+      {props.children}
+    </div>
   )
 }
 
