d535ef09b2b202f52abd703afd223e06edafa8f8
Andrii Lishchyna <andrej044@i.ua>
2023-06-10 13:07:58 -0400

    replace robotList function on App.js component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import {robots} from './robots'
 import 'tachyons';
+import App from './App.js'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const robotsList = robots.map(robot => {
-  return(
-    <Card id = {robot.id} 
-          name = {robot.name}
-          username = {robot.username}
-          email = {robot.email}
-          key={robot.id}
-    />  
-  )
-})
-
 root.render(
   <React.StrictMode>
-    {robotsList}
+    <App robots={robots}/>
   </React.StrictMode>
 );
 
