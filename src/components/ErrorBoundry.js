27d76cc2083c40e15690a5c9806b7f7ae0fd07ef
Andrii Lishchyna <andrej044@i.ua>
2023-06-11 14:03:13 -0400

    implement logic Error Boundry components for catch errors but going app work

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -4,5 +4,28 @@ import React, {Component} from 'react';
 export default class ErrorBoundry extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      hasError:false,
+      message:''
+    }
+  }
+
+  componentDidCatch(error) {
+      this.setState({
+        hasError:true,
+        message: error
+      })
+  }
+
+  render(){
+    if(this.state.hasError){
+      return (
+       <div className='tc'>
+        <h1>Something WRONG!!!!</h1>
+        <p>{this.state.message}</p>
+       </div> 
+      )
+    }
+    else return this.props.children
   }
 }
