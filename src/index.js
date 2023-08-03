0c1a965203bbf255b8ffd4355d76439cc0393266
Andrii Lishchyna <andrej044@i.ua>
2023-08-03 15:53:00 -0400

    install  @reduxjs/toolkit and import configureStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
