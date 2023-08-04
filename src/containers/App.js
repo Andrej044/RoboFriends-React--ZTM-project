e1978639e0812ef60dd1365b43bbafe143cff0f3
Andrii Lishchyna <andrej044@i.ua>
2023-08-04 19:16:19 -0400

    import action setSearchField

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,11 +4,11 @@ import SearchBox from '../components/SearchBox'
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css"
+import { setSearchField } from "../actions";
 
-const  App = () => {
+const  App = (props) => {
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchfield] = useState('');
-
   useEffect(()=>{
       fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json())
