bf912e74ceed438c6868c24e701aceb251dfeab0
Andrii Lishchyna <andrej044@i.ua>
2023-08-05 21:43:57 -0400

    implement asynchronous fetching data uses redux-thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,35 +5,40 @@ import SearchBox from '../components/SearchBox'
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css"
-import { setSearchField } from "../actions";
+import { setSearchField, requestRobots } from "../actions";
 
 const mapStateToProps = (state) => {
   return {
-    searchField:state.searchRobots.searchField
+    searchField:state.searchRobots.searchField,
+    robots:state.requestRobots.robots,
+    isPending:state.requestRobots.isPending,
+    error:state.requestRobots.error,
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => requestRobots(dispatch)
   }
 }
 
 const  App = (props) => {
-    const [robots, setRobots] = useState([]);
+    // const [robots, setRobots] = useState([]);
     
   useEffect(()=>{
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users => setRobots(users))
+      // fetch("https://jsonplaceholder.typicode.com/users")
+      //   .then(response => response.json())
+      //   .then(users => setRobots(users))
+      props.onRequestRobots();
   }, [])
 
-    const {searchField, onSearchChange} = props;
+    const {searchField, onSearchChange, robots, isPending} = props;
 
     const filteredList = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase())
       }) 
 
-      return !robots.length ?
+      return isPending ?
         <h1>Loading...</h1> :
         (
         <div className="tc">
