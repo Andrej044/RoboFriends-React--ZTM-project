388e83cc0c081dae36efa8de0706cdb3a0a548e2
Andrii Lishchyna <andrej044@i.ua>
2023-08-05 20:51:20 -0400

    define requestRobots action

--- src/actions.js
+++ src/actions.js
@@ -10,3 +10,19 @@ export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 })
+
+export const requestRobots = (dispatch) => {
+  dispatch({
+    type:REQUEST_ROBOTS_PENDING
+  });
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => dispatch({
+    type: REQUEST_ROBOTS_SUCCES,
+    payload:data
+  }))
+  .catch(error => dispatch({
+    type:REQUEST_ROBOTS_FAILED,
+    payload:error
+  }))
+}
