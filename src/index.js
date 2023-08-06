997b51bc2df893adee8f0f3c50fb1f0b720700a6
Andrii Lishchyna <andrej044@i.ua>
2023-08-05 20:37:26 -0400

    implement thunk function and applyied in applyMiddleware

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore} from '@reduxjs/toolkit';
 import {createLogger} from 'redux-logger';
-import { ThunkMiddleware } from 'redux-thunk';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App'
 import { searchRobots } from './reducers';
 
+const  thunk =  require('redux-thunk').default;
+
+
 const logger = createLogger();
 const store = configureStore({
   reducer: searchRobots,
-  middleware: (applyMiddleware) => applyMiddleware().concat(logger)
+  middleware: (applyMiddleware) => applyMiddleware(thunk).concat(logger)
 })
   
 const root = ReactDOM.createRoot(document.getElementById('root'));
